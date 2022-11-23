1324f98d7b04eebd27672ff1ed49973b6da82975
suraj807 <surajdev805@gmail.com>
2022-11-23 14:54:42 +0530

    Employe attendence

--- Employee Wage/Program.cs
+++ Employee Wage/Program.cs
@@ -6,8 +6,19 @@ namespace Employee_Wage
     {
         static void Main(string[] args)
         {
-             Console.WriteLine("welcome to empolyee wage");
-            
+            Console.WriteLine("*****************Welcome to EmployeeWage Computation*******************\n");
+            int fulltime = 1;
+            Random random = new Random();   // Creating object of Random class
+            int empChake = random.Next(0, 2);  //  Generating random number 0 or 1
+            if (empChake == fulltime)  //Checking that employee is present or not
+            {
+                Console.WriteLine("Employee is present"); // print 1
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");    // print 0
+            }
+
         }
     }
 }
