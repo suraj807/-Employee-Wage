a1ef066b468b2d0b23c78f9d13d52e18ff7b03f3
suraj807 <surajdev805@gmail.com>
2022-11-23 14:50:10 +0530

    employee wage

---
+++ Employee Wage/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Employee_Wage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("welcome to empolyee wage");
+            
+        }
+    }
+}
